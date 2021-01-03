80c2f6b501ca5b2448073567f3037250f6664d01
gilcoder <gilzamir@outlook.com>
2021-01-03 17:54:49 -0300

    bug fixed: run efficiently stable-baseline with custom environment starting script

---
+++ serverside/baseline/ext/EmoBoy/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour
+{
+
+
+
+    public Camera headViewCam;
+    public GameObject target;
+    public Button actionerSwapCam;
+
+    private Camera topViewCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        headViewCam.enabled = true;
+        topViewCam = GetComponent<Camera>();
+        topViewCam.enabled = false;
+        actionerSwapCam.onClick.AddListener(OnClick);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target.transform.position);
+    }
+
+    void OnClick(){
+        topViewCam.enabled = !topViewCam.enabled;
+        headViewCam.enabled = !headViewCam.enabled;
+    }
+}
