67ed3b5adee039997197a2421564bd69e83a5e34
Gilzamir Gomes (Kaos Koder) <54516662+gilcoder@users.noreply.github.com>
2022-11-06 10:13:30 -0300

    Update env.py
    id name refere to gym identifaction, so, rid refere to 'remote environment identification' (Unity or godot enviroment).

--- ai4upe/ai4u/AI4UEnv/envs/env.py
+++ ai4upe/ai4u/AI4UEnv/envs/env.py
@@ -13,26 +13,26 @@ class GenericEnvironment(gym.Env):
   """Custom Environment that follows gym interface"""
   metadata = {'render.modes': ['human']}
 
-  def __init__(self, controller_class=BasicGymController, id=None, server_IP="127.0.0.1", server_port=8080, sleep=0.1):
+  def __init__(self, controller_class=BasicGymController, rid=None, server_IP="127.0.0.1", server_port=8080, sleep=0.1):
     super(GenericEnvironment, self).__init__()
     # Define action and observation space
     # They must be gym.spaces objects
     # Example when using discrete actions:
-    if id is None:
-      id = ["0"]
-    elif type(id) is str:
-      id = [id]
-    elif type(id) is int:
-      id = [str(id)]
+    if rid is None:
+      rid = ["0"]
+    elif type(rid) is str:
+      rid = [rid]
+    elif type(rid) is int:
+      rid = [str(rid)]
     else:
-      raise TypeError("Unsupported type of ids parameter: ", type(id))
+      raise TypeError("Unsupported type of ids parameter: ", type(rid))
 
     controllers_classes =  [controller_class]
-    controller = startasdaemon(id, controllers_classes, server_IP, server_port, sleep)[0]
+    controller = startasdaemon(rid, controllers_classes, server_IP, server_port, sleep)[0]
     self.controller = controller
     self.action_space = self.controller.action_space
     self.observation_space = self.controller.observation_space
-    self.id =  id
+    self.rid =  rid
 
   def step(self, action):
     return self.controller.get_state(self.controller.request_step(action))
