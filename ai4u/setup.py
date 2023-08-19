62568b0e95847d3f10ab616f5bc8c14c6600f8c7
Gilzamir Gomes <54516662+gilzamir18@users.noreply.github.com>
2023-08-19 14:33:56 -0300

    Update setup.py

--- ai4u/setup.py
+++ ai4u/setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(name='ai4u',
         version='0.0.1',
-        install_requires=['numpy>=1.19.0', 'gym>=0.10.5', 'importlib-metadata<5.0', 'tensorboard>=2.0.0'],
+        install_requires=['numpy>=1.19.0', 'gym==0.25.2', 'importlib-metadata<5.0', 'tensorboard>=2.0.0'],
         py_modules=[]
 )
