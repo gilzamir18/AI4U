60a2ec0dd9b08c31399010418f6ef12d6c32affa
Gilzamir Gomes <54516662+gilzamir18@users.noreply.github.com>
2024-03-22 08:47:52 -0300

    Fix issue #21 (github)

--- pyplugin/ai4upy/ai4u/setup.py
+++ pyplugin/ai4upy/ai4u/setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='ai4u',
         version='0.0.1',
         install_requires=['numpy', 'gymnasium'],
-        py_modules=[]
+        py_modules=['ai4u']
 )
