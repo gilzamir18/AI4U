6b335fae683d9d2fa48b2e4dd0b855b8888248d5
Gilzamir Gomes <54516662+gilzamir18@users.noreply.github.com>
2024-04-12 14:10:38 -0300

    Documentation updated.

--- doc/trainer_example/scene_simplescene/appgym_sb3test.py
+++ doc/trainer_example/scene_simplescene/appgym_sb3test.py
@@ -1,21 +1,24 @@
-from bemaker.onnxutils import sac_export_to
-from bemaker.controllers import BasicGymController
-import BMEnv
-import  gymnasium  as gym
+import gymnasium as gym
 import numpy as np
 from stable_baselines3 import SAC
 from stable_baselines3.sac import MultiInputPolicy
+import ai4u
+from ai4u.controllers import BasicGymController
+from ai4u.onnxutils import sac_export_to
+import AI4UEnv
+import gymnasium as gym
 
-env = gym.make("BMEnv-v0")
+
+env = gym.make("AI4U-v0")
 
 print('''
 bemaker Client Controller
 =======================
 This example controll a movable character in game (unity or godot).
 ''')
-model = SAC.load("sac_bemaker")
+model = SAC.load("ai4u_model")
 
-sac_export_to("sac_bemaker", metadatamodel=env.controller.metadataobj)
+sac_export_to("ai4u_model", metadatamodel=env.controller.metadataobj)
 
 obs, info = env.reset()
 
