9c19f9f9991309ce82a7741f07c264e9a2170416
Gilzamir Gomes <54516662+gilzamir18@users.noreply.github.com>
2024-06-18 09:10:45 -0300

    RestlessRewardFunc2D updated

--- addons/ai4u/dotnet/scripts/RL/events/RestlessnessRewardFunc2D.cs
+++ addons/ai4u/dotnet/scripts/RL/events/RestlessnessRewardFunc2D.cs
@@ -20,6 +20,8 @@ public partial class RestlessnessRewardFunc2D : RewardFunc
     [Export]
     private float decreaseRate = -0.001f;
 
+    [Export(PropertyHint.Range, "-1, 1")]
+    private int ignoredAxis = -1;
     
     private BasicAgent basicAgent;
 
@@ -28,8 +30,14 @@ public partial class RestlessnessRewardFunc2D : RewardFunc
 
     private float acmReward = 0.0f;
 
+    public int IgnoredAxis { get => ignoredAxis; set => ignoredAxis = value; }
+
     public override void OnSetup(Agent agent)
     {
+        if (ignoredAxis != -1 && ignoredAxis != 0 &&  ignoredAxis != 1)
+        {
+            GD.PrintErr("Invalid range of the property IgnoredAx is. Try: -1 (no axis ignored), 0 (axis x ignored) or 1 (axis y ignored).");
+        }
         basicAgent = (BasicAgent)agent;
         basicAgent.OnStepEnd += CheckDist;
     }
@@ -37,7 +45,21 @@ public partial class RestlessnessRewardFunc2D : RewardFunc
     private void CheckDist(BasicAgent basicAgent)
     {
         currentPosition = ((PhysicsBody2D)this.basicAgent.GetAvatarBody()).Position;
-        float dist = currentPosition.DistanceTo(previousPosition);
+        float dist = 0;
+        if (ignoredAxis == -1)
+        {
+            dist = currentPosition.DistanceTo(previousPosition);
+        }
+        else 
+        if (ignoredAxis == 0)
+        {
+            dist = Math.Abs(currentPosition.Y - previousPosition.Y);
+        }
+        else
+        {
+            dist = Mathf.Abs(currentPosition.X - previousPosition.X);
+        }
+
         if (dist < minDist)
         {
             acmReward += reward;
