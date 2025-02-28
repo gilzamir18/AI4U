fd645cf3d5841b9986ad1fdaa9e25177c4295217
Gilzamir Gomes <54516662+gilzamir18@users.noreply.github.com>
2025-02-28 20:05:14 -0300

    Update env.py

--- pyplugin/ai4u/AI4UEnv/envs/env.py
+++ pyplugin/ai4u/AI4UEnv/envs/env.py
@@ -3,11 +3,12 @@ import gymnasium as gym
 from ai4u.controllers import BasicGymController
 from ai4u.appserver import startasdaemon
 from ai4u.utils import rid_generator
+import json
 import types
 
 class GenericEnvironment(gym.Env):
   """Custom Environment that follows gym interface"""
-  metadata = {}
+  metadataobj = None
   envidx = 0
   controllers = None
   def __init__(self, controller_class=BasicGymController, rid=None, event_callback=None, config=None, **kargs):
@@ -38,31 +39,31 @@ class GenericEnvironment(gym.Env):
       GenericEnvironment.controllers = {}
       controllers = startasdaemon(rid, controller_classes, config)
       for i in range(len(controllers)):
-        print("env ", rid[i])
         GenericEnvironment.controllers[rid[i]] = controllers[i]
-    
+      GenericEnvironment.envidx = 0
     self.rid = rid[GenericEnvironment.envidx]
     self.controller = GenericEnvironment.controllers[self.rid]
-    GenericEnvironment.envidx += 1
     self.event_callback = event_callback
     if "metadata" in config:
-      self.controller.metadata = config["metadata"]
+      self.controller.metadataobj = json.loads(config["metadata"])
       self.observation_space, self.action_space = BasicGymController.get_env_spaces(self.controller.metadata)
     elif "observation_space" in config and "action_space" in config:
       self.observation_space = config['observation_space']
       self.action_space = config['action_space']
     else:
       self.controller.request_config()
-      #print("env ", info, " was started!")
+      GenericEnvironment.metadataobj = self.controller.metadatamodel
+      print("env ", GenericEnvironment.envidx, " was started!")
+      print(GenericEnvironment.metadataobj)
       if self.controller.action_space is not None:
         self.action_space = self.controller.action_space
       else:
         self.action_space = None
-
       if self.controller.observation_space is not None:
         self.observation_space = self.controller.observation_space
       else:
         self.observation_space = None
+    GenericEnvironment.envidx += 1
 
   def step(self, action):
     info = self.controller.request_step(action)
